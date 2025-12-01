1d501e452f26338876e8f94f2241ead589b3aa3b
Jaehyuk Ryu <ryubsmile@gmail.com>
2025-12-01 18:33:05 -0500

    chore: add javadoc to StopRecording.java use case interactor

--- src/main/java/org/wavelabs/soundscope/use_case/stop_recording/StopRecording.java
+++ src/main/java/org/wavelabs/soundscope/use_case/stop_recording/StopRecording.java
@@ -5,6 +5,18 @@ import org.wavelabs.soundscope.infrastructure.Recorder;
 
 import javax.sound.sampled.AudioFormat;
 
+/**
+ * Use Case Interactor for StopRecording.
+ *
+ * <p> Contains the application business rules for the StopRecording use case,
+ * and implements the StopRecordingIB interface. </p>
+ *
+ * <p>This interactor terminates the audio recording by:</p>
+ * <ul>
+ *   <li>Using the StopRecordingDAI to update the recording state.</li>
+ *   <li>No Output Data</li>
+ * </ul>
+ */
 public class StopRecording implements StopRecordingIB {
     private final StopRecordingDAI stopRecordingDAO;
 
